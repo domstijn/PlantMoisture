1b55d60c1859ae8486df18d073784c28be739e56
stijndom <dom.stijn99@gmail.com>
2025-04-15 10:22:10 +0200

    adds background color to html body tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,16 @@
     import PlantInPot from "$lib/PlantInPot.svelte";
 
     let { data } = $props();
-
 </script>
 
-<PlantInPot datetime={data.props.lastDatetime} meanValue={data.props.mean}/>
+<PlantInPot datetime={data.props.lastDatetime} meanValue={data.props.mean} />
+
+<style>
+    :global(html, body) {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+        background-color: beige;
+    }
+    
+</style>
