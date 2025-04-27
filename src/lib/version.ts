5ed88c207ab0a37868264e884e48023903cbcb24
stijndom <dom.stijn99@gmail.com>
2025-04-27 15:51:50 +0200

    adds version number to top right

---
+++ src/lib/version.ts
@@ -0,0 +1,2 @@
+// src/lib/version.ts
+export const gitCommitHash = import.meta.env.VITE_GIT_COMMIT_HASH;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,31 @@
 <script>
     import "../app.css";
+    import { gitCommitHash } from '$lib/version';
 
     let { children } = $props();
+
 </script>
 
 {@render children()}
+
+<div class="version-container">
+    <span>v:{gitCommitHash}</span>
+</div>
+
+<style>
+    div.version-container {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        z-index: 1000;
+    }
+
+    div.version-container span {
+        background-color: var(--bg-color-gray-inv);
+        color: var(--text-info-color);
+        padding: 4px 8px;
+        border-radius: 6px;
+        font-size: 8px;
+        font-family: sans-serif;
+    }
+</style>
